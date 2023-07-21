2294dba5ac4c19348952dafa80219e991310a26d
Hyeseong-Myeong <a01025819869@gmail.com>
2023-07-21 09:18:41 +0900

    project setup

---
+++ src/test/java/com/hyeseong/springsecurity_prct/SpringSecurityPrctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeseong.springsecurity_prct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPrctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
